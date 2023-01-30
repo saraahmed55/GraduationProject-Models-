75125e8a81ca66b667c6d636bf166efd7a1c35ab
Rawan_Elbanna <ciaorona633@gmail.com>
2023-01-30 16:44:14 +0200

    first

---
+++ visual question answering (VQA) with ViLT/main.py
@@ -0,0 +1,23 @@
+import requests
+from PIL import Image 
+from transformers import ViltProcessor
+from transformers import ViltForQuestionAnswering 
+import torch
+
+url = "http://images.cocodataset.org/test-stuff2017/000000002558.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+text = "What color of traffic signal?"
+
+
+processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+encoding = processor(image, text, return_tensors="pt")
+  
+
+#load the ViLT model
+model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+
+outputs = model(**encoding)
+logits = outputs.logits
+idx = torch.sigmoid(logits).argmax(-1).item()
+print("Predicted answer:", model.config.id2label[idx])
+     
